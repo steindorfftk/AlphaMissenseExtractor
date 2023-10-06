c344a78d2c06e5c5ab7d071f8545733945976d07
steindorfftk <147119154+steindorfftk@users.noreply.github.com>
2023-10-06 20:01:13 -0300

    Add files via upload

---
+++ READ_ME.md
@@ -0,0 +1,7 @@
+#Obs
+This program uses data for canonical transcripts and hg38 only.
+
+#Setup
+1 - AlphaMissense_hg38.tsv and AlphaMissense_aa_substitutions.tsv (from https://zenodo.org/record/8360242) must be in the AlphaMissense_Data directory.
+2 - Gene list must be set in input section
+
---
+++ main.py
@@ -0,0 +1,99 @@
+from gprofiler import GProfiler
+
+def ENST_translator(gene_list):
+	gp = GProfiler(return_dataframe=False)
+	ENST_dic = {}
+	for gene in gene_list:
+		if gene not in ENST_dic.keys():
+			ENST_dic[gene] = []
+		tags = gp.convert(organism='hsapiens',
+			query=gene,
+			target_namespace='ENST')
+		for value in tags:
+			ENST_dic[gene].append(value['converted'][4:])
+	return ENST_dic				
+			
+def UNIPROT_translator(gene_list):
+	gp = GProfiler(return_dataframe=False)
+	UNIPROT_dic = {}
+	for gene in gene_list:
+		if gene not in UNIPROT_dic.keys():
+			UNIPROT_dic[gene] = []
+		tags = gp.convert(organism='hsapiens',
+			query=gene,
+			target_namespace='UNIPROT_GN_ACC')
+		for value in tags:
+			UNIPROT_dic[gene].append(value['converted'])
+	return UNIPROT_dic
+
+
+def gene_overview(gene_list):
+	pathogenicity_score = {}
+	with open('AlphaMissense_Data/AlphaMissense_gene_hg38.tsv','r') as text:
+		for line in text:
+			for gene, ids in gene_list.items():
+				for value in ids:
+					if value in line:
+						pathogenicity_score[gene] = line.split()[1]
+	with open('output/pathogenicity_scores.tsv','w') as texto:
+		for gene, value in pathogenicity_score.items():
+			texto.write(gene + ' , ' + value + '\n')
+
+
+def missense_data_extract(enst_list):
+	missense_data = {}
+	
+	for gene in enst_list.keys():
+		missense_data[gene] = [] 
+	
+	with open('AlphaMissense_Data/AlphaMissense_hg38.tsv','r') as text:
+		for line in text:
+			for gene, ids in enst_list.items():
+				for tag in ids:
+					if tag in line:
+						missense_data[gene].append(line)
+	
+	for gene, data in missense_data.items():
+		file_path = 'output/' + gene + '_missense.tsv'					
+		with open(file_path,'w') as text:
+			for value in data:
+				text.write(value)
+
+def aminoacid_subst_data_extract(uniprot_list):
+	aminoacid_data = {}
+	
+	for gene in uniprot_list.keys():
+		aminoacid_data[gene] = [] 
+	
+	with open('AlphaMissense_Data/AlphaMissense_aa_substitutions_hg38.tsv','r') as text:
+		for line in text:
+			for gene, ids in uniprot_list.items():
+				for tag in ids:
+					if tag in line:
+						aminoacid_data[gene].append(line)
+	
+	for gene, data in aminoacid_data.items():
+		file_path = 'output/' + gene + '_substitutions.tsv'				
+		with open(file_path,'w') as text:
+			for value in data:
+				text.write(value)
+
+
+			
+#Input
+input_gene_list = ['IDUA']
+
+#Identificators
+enst_ids = ENST_translator(input_gene_list)
+uniprot_ids = UNIPROT_translator(input_gene_list)
+
+#Pathogenicity per gene
+gene_overview(enst_ids)
+
+
+#Nucleotide missense variants data extracting
+missense_data_extract(enst_ids)
+
+#Nucleotide missense variants exporting (AlphaMissense_hg38.tsv)
+aminoacid_subst_data_extract(uniprot_ids)
+
