dd11480d165956cb9448de64736648c47279f3fa
steindorfftk <147119154+steindorfftk@users.noreply.github.com>
2023-10-07 01:45:59 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -29,16 +29,50 @@ def UNIPROT_translator(gene_list):
 
 def gene_overview(gene_list):
 	pathogenicity_score = {}
-	with open('AlphaMissense_Data/AlphaMissense_gene_hg38.tsv','r') as text:
-		for line in text:
-			for gene, ids in gene_list.items():
-				for value in ids:
-					if value in line:
-						pathogenicity_score[gene] = line.split()[1]
-	with open('output/pathogenicity_scores.tsv','w') as texto:
-		texto.write('Gene , Score')
-		for gene, value in pathogenicity_score.items():
-			texto.write(gene + ' , ' + value + '\n')
+	if genome == 'HG19':
+		with open('AlphaMissense_Data/AlphaMissense_gene_hg19.tsv','r') as text:
+			for line in text:
+				for gene, ids in gene_list.items():
+					for value in ids:
+						if value in line:
+							pathogenicity_score[gene] = line.split()[1]
+		with open('output/pathogenicity_scores_hg19.tsv','w') as texto:
+			texto.write('Gene , Score')
+			for gene, value in pathogenicity_score.items():
+				texto.write(gene + ' , ' + value + '\n')
+	elif genome == 'HG38':
+		with open('AlphaMissense_Data/AlphaMissense_gene_hg38.tsv','r') as text:
+			for line in text:
+				for gene, ids in gene_list.items():
+					for value in ids:
+						if value in line:
+							pathogenicity_score[gene] = line.split()[1]
+		with open('output/pathogenicity_scores_hg38.tsv','w') as texto:
+			texto.write('Gene , Score')
+			for gene, value in pathogenicity_score.items():
+				texto.write(gene + ' , ' + value + '\n')
+	elif genome == 'Both':
+		with open('AlphaMissense_Data/AlphaMissense_gene_hg19.tsv','r') as text:
+			for line in text:
+				for gene, ids in gene_list.items():
+					for value in ids:
+						if value in line:
+							pathogenicity_score[gene] = line.split()[1]
+		with open('output/pathogenicity_scores_hg19.tsv','w') as texto:
+			texto.write('Gene , Score')
+			for gene, value in pathogenicity_score.items():
+				texto.write(gene + ' , ' + value + '\n')
+		with open('AlphaMissense_Data/AlphaMissense_gene_hg38.tsv','r') as text:
+			for line in text:
+				for gene, ids in gene_list.items():
+					for value in ids:
+						if value in line:
+							pathogenicity_score[gene] = line.split()[1]
+		with open('output/pathogenicity_scores_hg38.tsv','w') as texto:
+			texto.write('Gene , Score')
+			for gene, value in pathogenicity_score.items():
+				texto.write(gene + ' , ' + value + '\n')
+					
 
 
 def missense_data_extract(enst_list):
@@ -46,57 +80,198 @@ def missense_data_extract(enst_list):
 	
 	for gene in enst_list.keys():
 		missense_data[gene] = [] 
+	if genome == 'HG19':
+		with open('AlphaMissense_Data/AlphaMissense_hg19.tsv','r') as text:
+			for line in text:
+				for gene, ids in enst_list.items():
+					for tag in ids:
+						if tag in line:
+							missense_data[gene].append(line)
+		
+		for gene, data in missense_data.items():
+			file_path = 'output/' + gene + '_missense_hg19.tsv'					
+			with open(file_path,'w') as text:
+				text.write('CHROM	POS	REF	ALT	genome	uniprot_id	transcript_id	protein_variant	am_pathogenicity	am_class \n')
+				for value in data:
+					text.write(value)
+	elif genome == 'HG38':
+		with open('AlphaMissense_Data/AlphaMissense_hg38.tsv','r') as text:
+			for line in text:
+				for gene, ids in enst_list.items():
+					for tag in ids:
+						if tag in line:
+							missense_data[gene].append(line)
+		
+		for gene, data in missense_data.items():
+			file_path = 'output/' + gene + '_missense_hg38.tsv'					
+			with open(file_path,'w') as text:
+				text.write('CHROM	POS	REF	ALT	genome	uniprot_id	transcript_id	protein_variant	am_pathogenicity	am_class \n')
+				for value in data:
+					text.write(value)
+	elif genome == 'Both':
+		with open('AlphaMissense_Data/AlphaMissense_hg19.tsv','r') as text:
+			for line in text:
+				for gene, ids in enst_list.items():
+					for tag in ids:
+						if tag in line:
+							missense_data[gene].append(line)
+		
+		for gene, data in missense_data.items():
+			file_path = 'output/' + gene + '_missense_hg19.tsv'					
+			with open(file_path,'w') as text:
+				text.write('CHROM	POS	REF	ALT	genome	uniprot_id	transcript_id	protein_variant	am_pathogenicity	am_class \n')
+				for value in data:
+					text.write(value)
+		with open('AlphaMissense_Data/AlphaMissense_hg38.tsv','r') as text:
+			for line in text:
+				for gene, ids in enst_list.items():
+					for tag in ids:
+						if tag in line:
+							missense_data[gene].append(line)
+		
+		for gene, data in missense_data.items():
+			file_path = 'output/' + gene + '_missense_hg38.tsv'					
+			with open(file_path,'w') as text:
+				text.write('CHROM	POS	REF	ALT	genome	uniprot_id	transcript_id	protein_variant	am_pathogenicity	am_class \n')
+				for value in data:
+					text.write(value)
 	
-	with open('AlphaMissense_Data/AlphaMissense_hg38.tsv','r') as text:
-		for line in text:
-			for gene, ids in enst_list.items():
-				for tag in ids:
-					if tag in line:
-						missense_data[gene].append(line)
-	
-	for gene, data in missense_data.items():
-		file_path = 'output/' + gene + '_missense.tsv'					
-		with open(file_path,'w') as text:
-			text.write('CHROM	POS	REF	ALT	genome	uniprot_id	transcript_id	protein_variant	am_pathogenicity	am_class \n')
-			for value in data:
-				text.write(value)
 
 def aminoacid_subst_data_extract(uniprot_list):
 	aminoacid_data = {}
 	
 	for gene in uniprot_list.keys():
 		aminoacid_data[gene] = [] 
-	
-	with open('AlphaMissense_Data/AlphaMissense_aa_substitutions_hg38.tsv','r') as text:
-		for line in text:
-			for gene, ids in uniprot_list.items():
-				for tag in ids:
-					if tag in line:
-						aminoacid_data[gene].append(line)
-	
-	for gene, data in aminoacid_data.items():
-		file_path = 'output/' + gene + '_substitutions.tsv'				
-		with open(file_path,'w') as text:
-			text.write('uniprot_id	protein_variant	am_pathogenicity	am_class \n')
-			for value in data:
-				text.write(value)
-
-
+	if genome == 'HG19':
+		with open('AlphaMissense_Data/AlphaMissense_aa_substitutions_hg19.tsv','r') as text:
+			for line in text:
+				for gene, ids in uniprot_list.items():
+					for tag in ids:
+						if tag in line:
+							aminoacid_data[gene].append(line)
+		
+		for gene, data in aminoacid_data.items():
+			file_path = 'output/' + gene + '_canonical_substitutions_hg19.tsv'				
+			with open(file_path,'w') as text:
+				text.write('uniprot_id	protein_variant	am_pathogenicity	am_class \n')
+				for value in data:
+					text.write(value)
+	elif genome == 'HG38':
+		with open('AlphaMissense_Data/AlphaMissense_aa_substitutions_hg38.tsv','r') as text:
+			for line in text:
+				for gene, ids in uniprot_list.items():
+					for tag in ids:
+						if tag in line:
+							aminoacid_data[gene].append(line)
+		
+		for gene, data in aminoacid_data.items():
+			file_path = 'output/' + gene + '_canonical_substitutions_hg38.tsv'				
+			with open(file_path,'w') as text:
+				text.write('uniprot_id	protein_variant	am_pathogenicity	am_class \n')
+				for value in data:
+					text.write(value)
+	elif genome == 'Both':
+		with open('AlphaMissense_Data/AlphaMissense_aa_substitutions_hg19.tsv','r') as text:
+					for line in text:
+						for gene, ids in uniprot_list.items():
+							for tag in ids:
+								if tag in line:
+									aminoacid_data[gene].append(line)
+		
+		for gene, data in aminoacid_data.items():
+			file_path = 'output/' + gene + '_canonical_substitutions_hg19.tsv'				
+			with open(file_path,'w') as text:
+				text.write('uniprot_id	protein_variant	am_pathogenicity	am_class \n')
+				for value in data:
+					text.write(value)
+		with open('AlphaMissense_Data/AlphaMissense_aa_substitutions_hg38.tsv','r') as text:
+			for line in text:
+				for gene, ids in uniprot_list.items():
+					for tag in ids:
+						if tag in line:
+							aminoacid_data[gene].append(line)
+		
+		for gene, data in aminoacid_data.items():
+			file_path = 'output/' + gene + '_canonical_substitutions_hg38.tsv'				
+			with open(file_path,'w') as text:
+				text.write('uniprot_id	protein_variant	am_pathogenicity	am_class \n')
+				for value in data:
+					text.write(value)		
 			
-#Input
-input_gene_list = ['IDUA']
+#Choose genome
+while True:
+    reference = input('Choose reference genome: (1) - HG19 ; (2) - HG38 ; (3) - Both > ')
+    
+    if reference in ['1', '2', '3']:
+        break
+    else:
+        print("Invalid choice. Please choose a reference genome: ")
+
+if reference == '1':
+	genome = 'HG19'
+elif reference == '2':
+	genome = 'HG38'
+elif reference == '3':
+	genome == 'Both'
+else:
+	print('Warning: no reference genome selected - Using both')
+	genome == 'Both'
+
+user_gene_list = []
+gene = input('Enter a gene and press enter > ').upper()
+user_gene_list.append(gene)
+
+phrase = 'Gene list: ' + user_gene_list[0]
+print(phrase)
+
+while True:
+	gene = input('Add another gene or send "E" to continue > ').upper()
+	if gene == 'E':
+		break
+	else:
+		user_gene_list.append(gene)
+		phrase += ' , '
+		phrase += gene
+		print(phrase)
+		
+input_gene_list = user_gene_list
+	
+print(phrase+'\n\n\n')
+
 
 #Identificators
+print("Searching for genes' ENSTs...")
 enst_ids = ENST_translator(input_gene_list)
+print('Done')
+print("Searching for genes' UNIPROT IDs...")
 uniprot_ids = UNIPROT_translator(input_gene_list)
+print('Done')
 
-#Pathogenicity per gene
+#a) Pathogenicity per gene
+print("Extracting the pathogenicity mean for list of genes...")
 gene_overview(enst_ids)
+print('Done')
 
-
-#Nucleotide missense variants data extracting
+#b) Nucleotide missense variants data extracting
+print("Extracting the predictions for missense variants in the cannonical transcript of each gene...")
 missense_data_extract(enst_ids)
+print('Done')
 
-#Nucleotide missense variants exporting (AlphaMissense_hg38.tsv)
+#c) Aminoacid substitutions 
+print("Extracting the predictions for aminoacid substitutions in the cannonical protein sequence of each gene...")
 aminoacid_subst_data_extract(uniprot_ids)
+print('Done')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
